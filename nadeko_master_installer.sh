f73a2a4ecb961b7feb7d45800a8455a91f6d6840
StrangeRanger <strangeranger01@gmail.com>
2020-09-22 15:58:34 -0700

    Move credentials code to its own script

--- nadeko_master_installer.sh
+++ nadeko_master_installer.sh
@@ -30,7 +30,7 @@
 		echo "6. Auto-Install pm2 (For pm2 information, see README!)"
 		echo "7. Start Nadeko in pm2 (Complete option 6 first!)"
 		echo "8. Exit"
-		read -p "Choose [1] to Download, [2 or 3] to Run, [6 and 7] for pm2 setup/startup (see README) or [8] to Exit." choice
+		read -p "Choose [1] to Download, [2 or 3] to Run, [6 and 7] for pm2 setup/startup (see README) or [8] to Exit. " choice
 		case "$choice" in
 			1)
 				clear -x
@@ -58,95 +58,8 @@
 				;;
 			5)
 				clear -x
-				read -p "We will now create a new credentials.json. Press [Enter] to continue."
-
-				echo -e "\n-------------"
-				echo "${cyan}This field is required and cannot be left blank${nc}"
-				while true; do
-					read -p "Enter your bot token: " clientid
-					if [[ -n $clientid ]]; then 
-						break
-					fi
-				done
-				echo "Client ID: $clientid"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				echo "${cyan}This field is required and cannot be left blank${nc}"
-				while true; do
-					read -p "Enter your bot token (it is not bot secret, it should be ~59 characters long): " token
-					if [[ -n $token ]]; then break; fi
-				done
-				echo "Bot token: $token"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				echo "${cyan}This field is required and cannot be left blank${nc}"
-				while true; do
-					read -p "Enter your own ID: " ownerid
-					if [[ -n $ownerid ]]; then break; fi
-				done
-				echo "Owner ID: $ownerid"
-				echo -e "-------------\n"
-				
-
-				echo -e "\n-------------"
-				read -p "Enter your Google API key: " googleapi
-				echo "Google API Key: $googleapi"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				read -p "Enter your Mashape Key: " mashapekey
-				echo "Mashape Key: $mashapekey"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				read -p "Enter your OSU API Key: " osu
-				echo "OSU API Key: $osu"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				read -p "Enter your Cleverbot API Key: " cleverbot
-				echo "Cleverbot API Key: $cleverbot"
-				echo -e "-------------\n"
-				
-				echo -e "\n-------------"
-				read -p "Enter your Twitch Client ID: " twitchcid
-				echo "Twitch Client ID: $twitchcid"
-				echo -e "-------------\n"
-
-				echo -e "\n-------------"
-				read -p "Enter your Location IQ API Key: " locationiqapi
-				echo "Location IQ API Key: $locationiqapi"
-				echo -e "-------------\n"
-				
-				echo -e "\n-------------"
-				read -p "Enter your Timezone DB API Key: " timedbapi
-				echo "Timezone DB API Key: $timedbapi"
-				echo -e "-------------\n"
-
-				echo "Backing up current 'credentials.json'..."
-				mv NadekoBot/src/NadekoBot/credentials.json NadekoBot/src/NadekoBot/credentials.json.bak
-				echo "Creating 'credentials.json'..."
-				echo "{
-	\"ClientId\": $clientid,
-	\"Token\": \"$token\",
-	\"OwnerIds\": [
-		$ownerid
-	],
-	\"GoogleApiKey\": \"$googleapi\",
-	\"MashapeKey\": \"$mashapekey\",
-	\"OsuApiKey\": \"$osu\",
-	\"CleverbotApiKey\": \"$cleverbot\",
-	\"TwitchClientId\": \"$twitchcid\",
-	\"LocationIqApiKey\": \"$locationiqapi\",
-	\"TimezoneDbApiKey\": \"$timedbapi\",
-	\"Db\": null,
-	\"TotalShards\": 1
-}" | cat - > NadekoBot/src/NadekoBot/credentials.json
-				
-				echo "${green}Finished creating 'credentials.json'${nc}"
-				read -p "Press [Enter] to return the the installer menu"
+				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/credentials_setup.sh
+				chmod +x credentials_setup.sh && ./credentials_setup
 				clear -x
 				;;
 			6)
