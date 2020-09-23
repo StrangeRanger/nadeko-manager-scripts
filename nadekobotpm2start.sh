bc2437bdf3809e0dc46f66934f6e9230ce2c95cc
StrangeRanger <strangeranger01@gmail.com>
2020-09-23 14:09:06 -0700

    No longer uses sudo to start pm2

--- nadekobotpm2start.sh
+++ nadekobotpm2start.sh
@@ -20,33 +20,33 @@ if [ $choice -eq 1 ] ; then
 	wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/NadekoARN.sh 
 	cd "$root"
 	echo "Starting Nadeko in pm2 with auto-restart and no auto-update..."
-	sudo pm2 start "$root/NadekoARN.sh" --interpreter=bash --name=Nadeko
-	sudo pm2 startup
-	sudo pm2 save
+	pm2 start "$root/NadekoARN.sh" --interpreter=bash --name=Nadeko
+	pm2 startup
+	pm2 save
 	echo ""
-	echo "If you did everything correctly, pm2 should have started up Nadeko! Please use sudo pm2 info Nadeko to check. You can view pm2 logs with sudo pm2 logs Nadeko"
+	echo "If you did everything correctly, pm2 should have started up Nadeko! Please use pm2 info Nadeko to check. You can view pm2 logs with pm2 logs Nadeko"
 else
 	if [ $choice -eq 2 ] ; then
 		echo ""
 		wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/NadekoARU_Latest.sh 
 		cd "$root"
 		echo "Starting Nadeko in pm2 with auto-restart and auto-update..."
-		sudo pm2 start "$root/NadekoARU_Latest.sh" --interpreter=bash --name=Nadeko
-		sudo pm2 startup
-		sudo pm2 save
+		pm2 start "$root/NadekoARU_Latest.sh" --interpreter=bash --name=Nadeko
+		pm2 startup
+		pm2 save
 		echo ""
-		echo "If you did everything correctly, pm2 should have started up Nadeko! Please use sudo pm2 info Nadeko to check. You can view pm2 logs with sudo pm2 logs Nadeko"
+		echo "If you did everything correctly, pm2 should have started up Nadeko! Please use pm2 info Nadeko to check. You can view pm2 logs with pm2 logs Nadeko"
 	else
 		if [ $choice -eq 3 ] ; then
 		echo ""
 		wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/nadeko_run.sh
 		cd "$root"
 		echo "Starting Nadeko in pm2 normally without any auto update or restart.."
-		sudo pm2 start "$root/nadeko_run.sh" --interpreter=bash --name=Nadeko
-		sudo pm2 startup
-		sudo pm2 save
+		pm2 start "$root/nadeko_run.sh" --interpreter=bash --name=Nadeko
+		pm2 startup
+		pm2 save
 		echo ""
-		echo "If you did everything correctly, pm2 should have started up Nadeko! Please use sudo pm2 info Nadeko to check. You can view pm2 logs with sudo pm2 logs Nadeko"	
+		echo "If you did everything correctly, pm2 should have started up Nadeko! Please use pm2 info Nadeko to check. You can view pm2 logs with pm2 logs Nadeko"	
 		else
 			if [ $choice -eq 4 ] ; then
 				echo ""
