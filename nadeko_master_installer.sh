2479125a8d750b629ae774aff8e0f459f4e41321
StrangeRanger <strangeranger01@gmail.com>
2020-09-22 17:36:23 -0700

    Prevents the execution of specific options based on predetermined 'things'

--- nadeko_master_installer.sh
+++ nadeko_master_installer.sh
@@ -22,29 +22,84 @@
 	echo ""
 
 	while true; do
-		echo "1. Download NadekoBot"
-		echo "2. Run Nadeko (Normally)"
-		echo "3. Run Nadeko with Auto Restart in this session"
-		echo "4. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
-		echo "5. Set up credentials.json (If you have downloaded NadekoBot already)"
-		echo "6. Auto-Install pm2 (For pm2 information, see README!)"
-		echo "7. Start Nadeko in pm2 (Complete option 6 first!)"
+		if (! hash git || ! hash dotnet) &>/dev/null; then
+			echo "1. Download NadekoBot ${red}(Disabled until prerequisites are installed)${nc}"
+			disabled_1="true"
+		else
+			echo "1. Download NadekoBot"
+			disabled_1="false"
+		fi
+		
+		if [[ ! -d NadekoBot/src/NadekoBot/ || (! hash git || ! hash dotnet || ! hash node || ! hash pm2) &>/dev/null || ! -d NadekoBot/src/NadekoBot/credentials.json ]]; then
+			echo "2. Run Nadeko (Normally) ${red}(Disabled until credentials.json, Nadeko, and prerequisites are installed)${nc}"
+			echo "3. Run Nadeko with Auto Restart in this session ${red}(Disabled until credentials.json, Nadeko, and prerequisites are installed)${nc}"
+			disabled_23="true"
+		else
+			echo "2. Run Nadeko (Normally)"
+			echo "3. Run Nadeko with Auto Restart in this session"
+			disabled_23="false"
+		fi
+
+		if [[ $distro = "Darwin" ]]; then
+			echo "4. Install prerequisites ${red}(Disabled due to being run on macOS)${nc}"
+			disabled_4="true"
+		else
+			echo "4. Install prerequisites"
+			disabled_4="false"
+		fi
+
+		if [[ ! -d NadekoBot/src/NadekoBot/ ]]
+			echo "5. Set up credentials.json ${red}(Disabled until Nadeko hash been downloaded)${nc}"
+			disabled_5="true"
+		else
+			echo "5. Set up credentials.json"
+			disabled_5="false"
+		fi
+
+		if ! hash node &>/dev/null; then
+			echo "6. Install pm2 ${red}(Disabled until prerequisites are installed)${nc}"
+			disabled_6="true"
+		else
+			echo "6. Install pm2"
+			disabled_6="false"
+		fi
+		
+		if [[ ! -d NadekoBot/src/NadekoBot/ || (! hash git || ! hash dotnet || ! hash node || ! hash pm2) &>/dev/null || ! -d NadekoBot/src/NadekoBot/credentials.json ]]; then
+			echo "7. Start Nadeko in pm2 ${red}(Disabled until credentials.json, Nadeko, and prerequisites are installed)${nc}"
+			disabled_7="true"
+		else
+			echo "7. Start Nadeko in pm2"
+			disabled_7="false"
+		fi
+
 		echo "8. Exit"
 		read -p "Choose [1] to Download, [2 or 3] to Run, [6 and 7] for pm2 setup/startup (see README) or [8] to Exit. " choice
 		case "$choice" in
 			1)
 				clear -x
+				if [[ $disabled_1 = "true" ]]; then
+					echo "${red}Option 1 is currently disabled${nc}"
+					continue
+				fi
 				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/nadeko_installer_latest.sh
 				chmod +x nadeko_installer_latest.sh && ./nadeko_installer_latest.sh
 				clear -x
 				;;
 			2)
 				clear -x
+				if [[ $disabled_23 = "true" ]]; then
+					echo "${red}Option 2 is currently disabled${nc}"
+					continue
+				fi
 				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/nadeko_run.sh
 				chmod +x nadeko_run.sh && ./nadeko_run.sh
 				clear -x
 				;;
 			3)
+				if [[ $disabled_23 = "true" ]]; then
+					echo "${red}Option 3 is currently disabled${nc}"
+					continue
+				fi
 				clear -x
 				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/NadekoAutoRestartAndUpdate.sh
 				chmod +x NadekoAutoRestartAndUpdate.sh && ./NadekoAutoRestartAndUpdate.sh
@@ -52,24 +107,40 @@
 				;;
 			4)
 				clear -x
+				if [[ $disabled_4 = "true" ]]; then
+					echo "${red}Option 4 is currently disabled${nc}"
+					continue
+				fi
 				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/nadekoautoinstaller.sh
 				chmod +x nadekoautoinstaller.sh && ./nadekoautoinstaller.sh
 				clear -x
 				;;
 			5)
 				clear -x
+				if [[ $disabled_5 = "true" ]]; then
+					echo "${red}Option 5 is currently disabled${nc}"
+					continue
+				fi
 				wget -qN https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/credentials_setup.sh
 				chmod +x credentials_setup.sh && ./credentials_setup
 				clear -x
 				;;
 			6)
 				clear -x
+				if [[ $disabled_6 = "true" ]]; then
+					echo "${red}Option 6 is currently disabled${nc}"
+					continue
+				fi
 				wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/nadekopm2setup.sh
 				chmod +x nadekopm2setup.sh && ./nadekopm2setup.sh
 				clear -x	
 				;;
 			7)
 				clear -x
+				if [[ $disabled_7 = "true" ]]; then
+					echo "${red}Option 7 is currently disabled${nc}"
+					continue
+				fi
 				wget -N https://github.com/Kwoth/NadekoBot-BashScript/raw/1.9/nadekobotpm2start.sh
 				chmod +x nadekobotpm2start.sh && ./nadekobotpm2start.sh
 				clear -x
