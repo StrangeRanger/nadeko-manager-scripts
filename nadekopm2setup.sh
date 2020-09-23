c151c0157eba115bd1524f647f5c1195a161a832
StrangeRanger <strangeranger01@gmail.com>
2020-09-23 11:59:27 -0700

    No longer installs specific version of pm2

--- nadekopm2setup.sh
+++ nadekopm2setup.sh
@@ -1,76 +1,6 @@
-#!/bin/bash -e
-root=$(pwd)
-echo ""
+#!/bin/bash
 
-function detect_OS_ARCH_VER_BITS {
-	ARCH=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')
-	if [ -f /etc/lsb-release ]; then
-	    . /etc/lsb-release
-	    OS=$DISTRIB_ID
-	    VER=$DISTRIB_RELEASE
-	elif [ -f /etc/debian_version ]; then
-	    OS=Debian  # XXX or Ubuntu??
-	    VER=$(cat /etc/debian_version)
-	    SVER=$( cat /etc/debian_version | grep -oP "[0-9]+" | head -1 )
-	elif [ -f /etc/centos-release ]; then
-		OS=CentOS
-		VER=$( cat /etc/centos-release | grep -oP "[0-9]+" | head -1 )
-	else
-	    OS=$(uname -s)
-	    VER=$(uname -r)
-	fi
-	case $(uname -m) in
-	x86_64)
-	    BITS=64
-	    ;;
-	i*86)
-	    BITS=32
-	    ;;
-	armv*)
-	    BITS=32
-	    ;;
-	*)
-	    BITS=?
-	    ;;
-	esac
-	case $(uname -m) in
-	x86_64)
-	    ARCH=x64  # or AMD64 or Intel64 or whatever
-	    ;;
-	i*86)
-	    ARCH=x86  # or IA32 or Intel32 or whatever
-	    ;;
-	*)
-	    # leave ARCH as-is
-	    ;;
-	esac
-}
-
-declare OS ARCH VER BITS
-
-detect_OS_ARCH_VER_BITS
-
-export OS ARCH VER BITS
-
-if [ "$BITS" = 32 ]; then
-	echo -e "Your system architecture is $ARCH which is unsupported to run Nadeko. \nYour OS: $OS \nOS Version: $VER"
-	echo
-	printf "\e[1;31mPlease check the NadekoBot self-hosting guide for alternatives.\e[0m\n"
-	rm nadekopm2setup.sh
-	exit 1
-fi
-
-
-echo -e "Welcome to NadekoBot's pm2 setup! \nWould you like to continue? \nYour OS: $OS \nOS Version: $VER \nArchitecture: $ARCH"
-
-while true; do
-    read -p "[y/n]: " yn
-    case $yn in
-        [Yy]* ) clear; echo Running NadekoBot pm2 Setup; sleep 2; break;;
-        [Nn]* ) echo Quitting...; rm nadekopm2setup.sh && exit;;
-        * ) echo "Couldn't get that please type [y] for Yes or [n] for No.";;
-    esac
-done
+read -p "We will now setup pm2. Press [Enter] to continue."
 
 if [ "$OS" = "Ubuntu" ]; then
 echo "This installer will download/update NodeJS/npm and install pm2."
@@ -84,7 +14,7 @@ read -n 1 -s -p "Press any key to continue..."
 	sudo apt-get install -y build-essential
 	sudo npm i -g npm
 	echo "Installing pm2..."
-	sudo npm install pm2@3.1.3 -g
+	sudo npm install pm2 -g
 fi
 	
 if [ "$OS" = "Debian" ]; then
@@ -99,7 +29,7 @@ read -n 1 -s -p "Press any key to continue..."
 	sudo apt-get install -y build-essential
 	sudo npm i -g npm
 	echo "Installing pm2..."
-	sudo npm install pm2@3.1.3 -g
+	sudo npm install pm2 -g
 fi
 	
 if [ "$OS" = "LinuxMint" ]; then
@@ -114,7 +44,7 @@ read -n 1 -s -p "Press any key to continue..."
 	sudo apt-get install -y build-essential
 	sudo npm i -g npm
 	echo "Installing pm2..."
-	sudo npm install pm2@3.1.3 -g
+	sudo npm install pm2 -g
 fi
 
 if [ "$OS" = "CentOS" ]; then
@@ -129,7 +59,7 @@ read -n 1 -s -p "Press any key to continue..."
 	sudo yum install gcc-c++ make
 	sudo npm i -g npm
 	echo "Installing pm2..."
-	sudo npm install pm2@3.1.3 -g
+	sudo npm install pm2 -g
 fi
 
 
