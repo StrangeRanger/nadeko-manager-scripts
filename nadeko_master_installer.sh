4df4d7c9da07cde9221035680802464d9d86d02f
Hunter T <strangeranger01@randomserver.xyz>
2021-04-27 14:42:23 -0700

    Rewrote master installer with the skeleton of the actual file

--- nadeko_master_installer.sh
+++ nadeko_master_installer.sh
@@ -1,218 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Welcome to NadekoBot."
-root=$(pwd)
-echo ""
-choice=9
-echo "1. Download NadekoBot"
-echo "2. Run Nadeko (Normally)"
-echo "3. Run Nadeko with Auto Restart in this session"
-echo "4. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
-echo "5. Set up credentials.json (If you have downloaded NadekoBot already)"
-echo "6. Auto-Install pm2 (For pm2 information, see README!)"
-echo "7. Start Nadeko in pm2 (Complete option 6 first!)"
-echo "8. Exit"
-echo -rn "Choose [1] to Download, [2 or 3] to Run, [6 and 7] for pm2 setup/startup (see README) or [8] to Exit."
-while [ $choice -eq 9 ]; do
-    read choice
-    if [ $choice -eq 1 ]; then
-        echo ""
-        echo "Downloading NadekoBot, please wait."
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/nadeko_installer_latest.sh && bash "$root/nadeko_installer_latest.sh"
-        echo ""
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 2 ]; then
-        echo ""
-        echo "Running Nadeko Normally, if you are running this to check Nadeko, use .die command on discord to stop Nadeko."
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/nadeko_run.sh && bash "$root/nadeko_run.sh"
-        echo ""
-        echo "Welcome back to NadekoBot."
-        sleep 2s
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 3 ]; then
-        echo ""
-        echo "Running Nadeko with Auto Restart you will have to close the session to stop the auto restart."
-        sleep 5s
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/NadekoAutoRestartAndUpdate.sh && bash "$root/NadekoAutoRestartAndUpdate.sh"
-        echo ""
-        echo "That did not work?"
-        sleep 2s
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 4 ]; then
-        echo ""
-        echo "Getting the Auto-Installer for Debian/Ubuntu"
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/nadekoautoinstaller.sh && bash "$root/nadekoautoinstaller.sh"
-        echo ""
-        echo "Welcome back..."
-        sleep 2s
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 5 ]; then
-        echo ""
-        echo
-        echo -e "Let's begin creating a new credentials.json file if you are about to run the NadekoBot for the first time. \n \nPlease read JSON Explanations in the guide... \n \nPress [Y] when you are ready to continue or [N] to exit."
-        while true; do
-            read -p "[y/n]: " yn
-            case $yn in
-            [Yy]*)
-                clear
-                break
-                ;;
-            [Nn]*)
-                echo Exiting...
-                exit
-                ;;
-            *) echo "Couldn't get that please type [y] for Yes or [n] for No." ;;
-            esac
-        done
-        clear
-        cd "$root/NadekoBot/src/NadekoBot"
-        mv credentials.json credentials.json.old
-
-        echo Please enter your bot client ID:
-        echo ""
-        read clientid
-        echo ""
-        echo Alright saved \'$clientid\' as your client ID.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo Please enter your bot token \(It is not bot secret, it should be ~59 characters long.\):
-        echo ""
-        read token
-        echo ""
-        echo Alright saved \'$token\' as your bot\'s token.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo Please enter your own ID \(Refer to the guide, it will be bot\'s owner ID.\):
-        echo ""
-        read ownerid
-        echo ""
-        echo Alright saved \'$ownerid\' as owner\'s ID.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo Please enter Google API key \(Refer to the guide.\):
-        echo ""
-        read googleapi
-        echo ""
-        echo Alright saved \'$googleapi\' as your bot\'s Google API Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter Mashape Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read mashapekey
-        echo ""
-        echo Alright saved \'$mashapekey\' as your bot\'s Mashape Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter OSU API Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read osu
-        echo ""
-        echo Alright saved \'$osu\' as your bot\'s OSU API Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter Cleverbot API Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read cleverbot
-        echo ""
-        echo Alright saved \'$cleverbot\' as your bot\'s Cleverbot API Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter Twitch Client ID or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read twitchcid
-        echo ""
-        echo Alright saved \'$twitchcid\' as your bot\'s Twitch Client ID.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter Location IQ Api Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read locationiqapi
-        echo ""
-        echo Alright saved \'$locationiqapi\' as your bot\'s Location IQ API Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo -e "Please enter Timezone DB Api Key or Just Press [Enter Key] to skip. (optional) \nRefer to the JSON Explanations guide:"
-        echo ""
-        read timedbapi
-        echo ""
-        echo Alright saved \'$timedbapi\' as your bot\'s Timezone DB API Key.
-        echo ""
-        echo "----------"
-        echo ""
-
-        echo "{
-\"ClientId\": $clientid,
-\"Token\": \"$token\",
-\"OwnerIds\": [
-$ownerid
-],
-\"GoogleApiKey\": \"$googleapi\",
-\"MashapeKey\": \"$mashapekey\",
-\"OsuApiKey\": \"$osu\",
-\"CleverbotApiKey\": \"$cleverbot\",
-\"TwitchClientId\": \"$twitchcid\",
-\"LocationIqApiKey\": \"$locationiqapi\",
-\"TimezoneDbApiKey\": \"$timedbapi\",
-\"Db\": null,
-\"TotalShards\": 1
-}" | cat - >>credentials.json
-        echo Credentials setup completed.
-        sleep 5
-        clear
-        cd "$root"
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 6 ]; then
-        echo ""
-        echo "Starting the setup for pm2 with NadekoBot. This only has to be done once."
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/nadekopm2setup.sh && bash "$root/nadekopm2setup.sh"
-        echo ""
-        echo "Welcome back..."
-        sleep 2s
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 7 ]; then
-        echo ""
-        echo "Getting the pm2 startup options for NadekoBot.."
-        wget -N https://github.com/"$installer_repo"/"$installer_branch"/nadekobotpm2start.sh && bash "$root/nadekobotpm2start.sh"
-        echo ""
-        sleep 2s
-        bash "$root/linuxAIO.sh"
-    elif [ $choice -eq 8 ]; then
-        echo ""
-        echo "Exiting..."
-        cd "$root"
-        exit 0
-    else
-        clear
-        echo "1. Download NadekoBot"
-        echo "2. Run Nadeko (Normally)"
-        echo "3. Run Nadeko with Auto Restart in this session"
-        echo "4. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
-        echo "5. Set up credentials.json (If you have downloaded NadekoBot already)"
-        echo "6. Auto-Install pm2 (For pm2 information, see README!)"
-        echo "7. Start Nadeko in pm2 (Complete option 6 first!)"
-        echo "8. Exit"
-        echo -rn "Choose [1] to Download, [2 or 3] to Run, [6 and 7] for pm2 setup/startup (see README) or [8] to Exit."
-        choice=9
-    fi
+while true; do
+    echo "Welcome to NadekoBot."
+    echo ""
+    echo "1. Download NadekoBot"
+    echo "2. Run NadekoBot in the background"
+    echo "3. Run NadekoBot in the background with auto restart"
+    echo "4. Stop NadekoBot"
+    echo "5. Display '$nadeko_service_name' logs in follow mode"
+    echo "6. Install prerequisites"
+    echo "7. Set up credentials.json"
+    echo "8. Exit"
+    read -r choice
+    case "$choice" in
+        1)
+            # Downloads 'nadeko_latest_installer.sh' and executes it
+            ;;
+        2)
+            # Executes function to start/restart NadekoBot in the background, 
+            # using a daemon service
+            ;;
+        3)
+            # Executes function to start/restart NadekoBot in the background
+            # with auto restart, using a daemon service
+            ;;
+        4)
+            # Executes code to stop nadeko.service
+            ;;
+        5)
+            # Executes code to monitor and see the output of NadekoBot/'nadeko.service'
+            ;;
+        6)
+            # Downloads 'prereqs_installer.sh' and executes it
+            ;;
+        7)
+            # Downloads 'credentials_setup.sh' and executes it
+            ;;
+        8)
+            clean_exit "0" "Exiting"
+            ;;
+        *)
+            clear -x
+            echo "${red}Invalid input: '$choice' is not a valid option$nc" >&2
+            ;;
+    esac
 done
-
-cd "$root"
-exit 0
