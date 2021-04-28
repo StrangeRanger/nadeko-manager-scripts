fb610483a480b08056609e0c94af3666d06239e4
Hunter T <strangeranger01@randomserver.xyz>
2021-04-28 14:40:13 -0700

    Update comments, code style, and code

--- installer_prep.sh
+++ installer_prep.sh
@@ -6,12 +6,13 @@
 #
 # Note: All variables not defined in this script, are exported from
 # 'linuxAIO.sh'.
-#
-################################################################################
+# 
+########################################################################################
 #### [ Exported and/or Globally Used Variables ]
 
 
-# Used to check if 'linuxAIO.sh' is up to date.
+# Used to keep track of changes to 'linuxAIO.sh'.
+# Refer to the '[ Prepping ]' section of this script for more information.
 current_linuxAIO_revision="9"
 
 export yellow=$'\033[1;33m'
@@ -30,16 +31,41 @@ fi
 
 
 #### End of [ Exported and/or Globally Used Variables ]
-################################################################################
+########################################################################################
 #### [ Error Traps ]
 
 
-# Makes it possible to cleanly exit the installer by cleaning up files that
-# aren't required unless currently being run.
 clean_exit() {
-    echo ""
-    # Removes files that don't need to stay on the system unless being run,
-    # then exits the installer
+    ####
+    # FUNCTION INFO:
+    #
+    # Cleanly exit the installer by removing files that aren't required unless
+    # the installer is currently being run.
+    #
+    # @param $1 Exit status code.
+    # @param $2 Output text.
+    # @param $3 Determines if 'Cleaning up...' needs to be printed with a
+    #           new-line symbol.
+    ####
+
+    # Files to be removed.
+    local installer_files=("credentials_setup.sh" "installer_prep.sh"
+        "prereqs_installer.sh" "nadeko_latest_installer.sh"
+        "nadeko_master_installer.sh")
+
+    if [[ $3 = true ]]; then
+        echo "Cleaning up..."
+    else
+        echo -e "\nCleaning up..."
+    fi
+
+    ## Removes files specified in 'installer_files'.
+    for file in "${installer_files[@]}"; do
+        if [[ -f $file ]]; then rm "$file"; fi
+    done
+
+    echo "$2..."
+    exit "$1"
 }
 
 # Executes when the user uses 'Ctrl + Z' or 'Ctrl + C'.
@@ -49,38 +75,67 @@ trap 'echo -e "\n\nScript forcefully stopped"
 
 
 #### End of [ Error Traps ]
-################################################################################
+########################################################################################
 #### [ Prepping ]
 
 
-# Downloads latest version of 'linuxAIO.sh' if $linuxAIO_revision and 
-# $current_linuxAIO_revision aren't of equal value.
+## Downloads latest version of 'linuxAIO.sh' if $linuxAIO_revision and 
+## $current_linuxAIO_revision aren't of equal value.
 if [[ $linuxAIO_revision != "$current_linuxAIO_revision" ]]; then
-    echo ""
-    # Refer to comment above if statement for explanation
+    ####
+    # MORE INFO:
+    #
+    # Since 'linuxAIO.sh' remains on the user's system, any changes to the code
+    # that are pushed to github are never applied. whenever this
+    # $linuxAIO_revision and $current_linuxAIO_revision do not match, the newest
+    # version of 'linuxAIO.sh' is retrieved from github.
+    ####
+
+    # Save the value of 'installer_branch' specified in 'linuxAIO.sh', to be set
+    # in the new 'linuxAIO.sh'.
+    installer_branch=$(grep 'export installer_branch=' linuxAIO.sh | awk -F '"' '{print $2}');
+
+    echo "$yellow'linuxAIO.sh' is not up to date$nc"
+    echo "Downloading latest 'linuxAIO.sh'..."
+    curl "$raw_url"/linuxAIO.sh -o linuxAIO.sh || {
+        echo "${red}Failed to download latest 'linuxAIO.sh'...$nc" >&2
+        clean_exit "1" "Exiting" "true"
+    }
+
+    echo "Applying set configurations to 'linuxAIO.sh'..."
+    sed -i "s/export installer_branch=.*/export installer_branch=\"$installer_branch\"/" linuxAIO.sh
+    sudo chmod +x linuxAIO.sh  # Set execution permission
+    echo "${cyan}Re-execute 'linuxAIO.sh' to continue$nc"
+    # TODO: Figure out a way to get exec to work, instead of exiting script
+    clean_exit "0" "Exiting" "true"
 fi
 
-# Changes the working directory to the location of the executed scrpt.
+# Change the working directory to the location of the executed scrpt.
 cd "$(dirname "$0")" || {
     echo "${red}Failed to change working directory" >&2
     echo "${cyan}Change your working directory to that of the executed" \
         "script$nc"
     clean_exit "1" "Exiting" "true"
 }
-export root_dir="$PWD"
-export installer_prep="$root_dir/installer_prep.sh"
+
+export working_dir="$PWD"
+export installer_prep="$working_dir/installer_prep.sh"
 
 
 #### End of [ Prepping ]
-################################################################################
+########################################################################################
 #### [ Functions ]
 
 
-# Identify the operating system, version number, architecture, bit type (32
-# or 64), etc.
 detect_sys_info() {
-    arch=$(uname -m | sed 's/x86_//;s/i[3-6]86/32/')  # TODO: Remove???
+    ####
+    # FUNCTION INFO:
+    #
+    # Identify the operating system, version number, architecture, bit type (32
+    # or 64), etc.
+    ####
 
+    ## For Linux
     if [[ -f /etc/os-release ]]; then
         . /etc/os-release
         distro="$ID"
@@ -88,6 +143,7 @@ detect_sys_info() {
         sver=${ver//.*/}   # Version: x
         pname="$PRETTY_NAME"
         codename="$VERSION_CODENAME"
+    ## For macOS
     else
         distro=$(uname -s)
         if [[ $distro = "Darwin" ]]; then
@@ -108,12 +164,16 @@ detect_sys_info() {
     esac
 }
 
-# Download and execute 'nadeko_master_installer.sh'.
 execute_master_installer() {
+    ####
+    # FUNCTION INFO:
+    #
+    # Download and execute 'nadeko_master_installer.sh'.
+    ####
+
     supported=true
 
-    curl -s https://raw.githubusercontent.com/"$installer_repo"/"$installer_branch"/nadeko_master_installer.sh \
-            -o nadeko_master_installer.sh || {
+    curl -s "$raw_url"/nadeko_master_installer.sh -o nadeko_master_installer.sh || {
         echo "${red}Failed to download 'nadeko_master_installer.sh'$nc" >&2
         clean_exit "1" "Exiting" "true"
     }
@@ -125,7 +185,7 @@ execute_master_installer() {
 
 
 #### End of [ Functions ]
-################################################################################
+########################################################################################
 #### [ Main ]
 
 
@@ -139,14 +199,14 @@ echo "SYSTEM INFO"
 echo "Bit Type: $bits"
 echo "Architecture: $arch"
 printf "Distro: "
-# Checks if variable 'pname' exists.
+# Use $distro if $pname is empty.
 if [[ -n $pname ]]; then echo "$pname"; else echo "$distro"; fi
 echo "Distro Version: $ver"
 echo ""
 
-# Checks if Nadeko and installer are compatible with the operating system.
+## Checks if Nadeko and installer are compatible with the operating system.
 if [[ $distro = "ubuntu" ]]; then
-    if [[ $bits = 64 ]]; then  # B.1. Forcing 64 bit architecture
+    if [[ $bits = 64 ]]; then  # A.1. Forcing 64 bit architecture
         case "$ver" in
             16.04) execute_master_installer ;;
             18.04) execute_master_installer ;;
@@ -157,7 +217,7 @@ if [[ $distro = "ubuntu" ]]; then
         supported=false
     fi
 elif [[ $distro = "debian" ]]; then
-    if [[ $bits = 64 ]]; then  # B.1.
+    if [[ $bits = 64 ]]; then  # A.1.
         case "$sver" in
             9)  execute_master_installer ;;
             10) execute_master_installer ;;
@@ -167,7 +227,7 @@ elif [[ $distro = "debian" ]]; then
         supported=false
     fi
 elif [[ $distro = "linuxmint" ]]; then
-    if [[ $bits = 64 ]]; then  # B.1.
+    if [[ $bits = 64 ]]; then  # A.1.
         case "$sver" in
             18) execute_master_installer ;;
             19) execute_master_installer ;;
@@ -188,13 +248,13 @@ else
     supported=false
 fi
 
-# Provides the user with the option to continue, even if their system isn't
-# officially supported.
+## Provides the user with the option to continue, even if their system isn't officially
+## supported.
 if [[ $supported = false ]]; then
-    echo "${red}Your operating system/Linux Distribution is not OFFICIALLY" \
-        "supported by the installation, setup, and/or use of NadekoBot$nc" >&2
+    echo "${red}Your operating system/Linux Distribution is not OFFICIALLY supported" \
+        "the installation, setup, and/or use of NadekoBot$nc" >&2
     read -rp "Would you like to continue with the installation anyways? [y/N] " choice
-    choice=$(echo "$choice" | tr '[A-Z]' '[a-z]')
+    choice=$(echo "$choice" | tr '[A-Z]' '[a-z]')  # Convert user input to lowercase.
     case "$choice" in
         y|yes) clear -x; execute_master_installer ;;
         n|no)  clean_exit "0" "Exiting" ;;
@@ -204,4 +264,4 @@ fi
 
 
 #### End of [ Main ]
-################################################################################
+########################################################################################
