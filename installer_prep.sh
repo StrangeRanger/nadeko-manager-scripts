378714d0d1c2aaa71cd000081b9d51c1a6d1a4d1
Hunter T <strangeranger01@randomserver.xyz>
2022-08-23 08:16:37 -0700

    Final changes and fixes

--- installer_prep.sh
+++ installer_prep.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
+#
+# Acts as a transition script from linuxAIO revision 36 to 37. During these changes,
+# many of the files were renamed and had the file extension ('.sh') removed.
+#
+########################################################################################
+#### [ Variables ]
+
 
 ## Modify output text color.
-_GREEN="$(printf '\033[0;32m')"
-_CYAN="$(printf '\033[0;36m')"
-_RED="$(printf '\033[1;31m')"
-_NC="$(printf '\033[0m')"
+cyan="$(printf '\033[0;36m')"
+red="$(printf '\033[1;31m')"
+nc="$(printf '\033[0m')"
 
 
-echo -n "${_CYAN}Several scripts have been renamed, and require special intervention." \
-    "When the installer has exited, re-execute the installer and re-run Nadeko in" \
-    "your chosen mode. "
-read -rp "Press [Enter] to continue.${_NC}"
+#### End of [ Variables ]
+########################################################################################
+#### [ Main ]
 
-## Save the values of the current Configuration Variables specified in 'linuxAIO', to be
-## set in the new 'linuxAIO'.
+
+echo -n "${cyan}There've been some changes that require special intervention. When" \
+    "the installer has exited, re-execute the installer and re-run NadekoBot in your" \
+    "chosen run mode. "
+read -rp "Press [Enter] to continue.${nc}"
+
+## Save the values of the current Configuration Variables specified in 'linuxAIO.sh', to
+## be set in 'linuxAIO'.
 installer_branch=$(grep '^installer_branch=.*' linuxAIO.sh)  # A.1.
 installer_branch_found="$?"	                                 # A.1.
 nadeko_install_version=$(grep '^export _NADEKO_INSTALL_VERSION=.*' linuxAIO.sh)  # A.2.
 nadeko_install_version_found="$?"                                                # A.2.
 
 curl -O "$_RAW_URL"/linuxAIO && sudo chmod +x linuxAIO
+echo "Applying existing configurations to 'linuxAIO'..."
 
-echo "Applying existing configurations to the new 'linuxAIO'..."
-
-## Set $installer_branch inside of the new 'linuxAIO'.
+## Set $installer_branch inside of 'linuxAIO'.
 [[ $installer_branch_found = 0 ]] \
     && sed -i "s/^installer_branch=.*/$installer_branch/" linuxAIO
-
-## Set $nadeko_install_version inside of the new 'linuxAIO'.
+## Set $nadeko_install_version inside of 'linuxAIO'.
 [[ $nadeko_install_version_found = 0 ]] \
     && sed -i "s/^export _NADEKO_INSTALL_VERSION=.*/$nadeko_install_version/" linuxAIO
 
@@ -37,11 +46,12 @@ echo "Cleaning up..."
 [[ -f installer_prep.sh ]] && rm installer_prep.sh
 
 if [[ -f linuxAIO.sh && -f linuxAIO ]]; then
-    echo "Deleting 'linuxAIO.sh'..."
     rm linuxAIO.sh
 else
-    echo "${_RED}'linuxAIO.sh' and 'linuxAIO' should exist, but one or both do not.${_NC}"
+    echo "${red}'linuxAIO.sh' and 'linuxAIO' should exist, but one or both do not.${nc}"
     exit 1
 fi
 
-exit 0
+
+#### End of [ Main ]
+########################################################################################
