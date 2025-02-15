7969fc32bd37f1f2c5596e5a1163e49e3222c7bc
Hunter T. <strangeranger@hthompson.dev>
2025-02-14 20:15:54 -0800

    refactor: switch output text for when 'ccze' not installed

--- n-prereqs.bash
+++ n-prereqs.bash
@@ -227,7 +227,7 @@ install_ccze_arch() {
         read -rp "${E_NOTE}Would you like to continue? [y/N] " confirm
         confirm=${confirm,,}
         if [[ ! $confirm =~ ^y ]]; then
-            echo "${E_ERROR}Installation of 'ccze' and required build tools aborted"
+            echo "${E_NOTE}Installation of 'ccze' and required build tools aborted"
             echo "${E_WARN}'ccze' is required to colorize NadekoBot's logs"
             return 1
         fi
