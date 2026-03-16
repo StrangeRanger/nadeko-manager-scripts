ff3ee529c4c1152849a9a6d73349042caa020e43
Hunter T. <strangeranger@hthompson.dev>
2026-03-15 18:58:00 -0700

    Modify IFS

--- n-shared.bash
+++ n-shared.bash
@@ -59,7 +59,7 @@ E_FINISH_MENU_EXIT() {
     local trap_signals="${1:-EXIT SIGINT SIGHUP SIGTERM}"
     local prompt_message="${2:-${E_NOTE}Press [Enter] to return to the Manager menu}"
     local -a trap_signal_array
-    local IFS=$' \t\n'
+    local IFS=' '
 
     # Remove traps to prevent re-entry after cleanup.
     read -r -a trap_signal_array <<< "$trap_signals"
